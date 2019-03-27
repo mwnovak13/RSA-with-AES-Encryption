e9ad1eabde207f20689cefbaff36e4da4bf0d66c
User <User@DESKTOP-S3PIPTP>
2019-03-26 22:22:34 -0400

    Edits to encryptor.c. -Added generateKey -Removed some unused code

--- encryptor.c
+++ encryptor.c
@@ -4,7 +4,7 @@
 
 #include "aes_e.h"
 
-void deriveKeyFromPassphrase(char *, char *);
+
 void readPlainText(uint8_t *);
 void writeCipherText(uint8_t *);
 
@@ -13,17 +13,15 @@ int main(void)
 {
     uint8_t key[16] =        { (uint8_t) 0x2b, (uint8_t) 0x7e, (uint8_t) 0x15, (uint8_t) 0x16, (uint8_t) 0x28, (uint8_t) 0xae, (uint8_t) 0xd2, (uint8_t) 0xa6, (uint8_t) 0xab, (uint8_t) 0xf7, (uint8_t) 0x15, (uint8_t) 0x88, (uint8_t) 0x09, (uint8_t) 0xcf, (uint8_t) 0x4f, (uint8_t) 0x3c };
        
-    char passphrase [16];
-  
-    memset(passphrase, 0, sizeof(passphrase));
+
     
     printf("Acme Corporation AES Encryptor System\n");
     printf("\nEncrypting file plaintext.dat\n");
     printf("\nEnter passphrase: ");
     
-    fgets(passphrase, sizeof(passphrase), stdin);
+   
 
-    deriveKeyFromPassphrase(passphrase, key); 
+    generateKey(key); 
     
     uint8_t in[]  = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
     
@@ -42,7 +40,17 @@ int main(void)
     return 0;
 }
 
+void generateKey{
 
+unsigned char buf[16];
+	int i;
+	srand(time(NULL));
+		for (i = 0; i < sizeof(buf); i++) {
+			buf[i] = rand() % 256;
+		}
+}
+
+/*
 void deriveKeyFromPassphrase(char *passphrase, char *key) {
     int i, j, k;
     
@@ -73,4 +81,4 @@ void writeCipherText(uint8_t * ctxt) {
     fclose(fp);
 }
 
-
+*/
